12b2053f8232adbdc8a876a3819b051ad9063efe
ridho <ridho.putra@btpn.com>
2023-01-06 16:13:51 +0700

    add auto retry for join room due to 53113 room exist

--- app/server.js
+++ app/server.js
@@ -29,21 +29,37 @@ const findOrCreateRoom = async (roomName) => {
         await twilioClient.video.rooms(roomName).fetch();
         console.log(`joined the room #1`);
     } catch (error) {
+        console.log(`catch joined the room`);
         // the room was not found, so create it
         if (error.code == 20404) {
-            await twilioClient.video.rooms.create({
-                uniqueName: roomName,
-                type: "go",
-            });
+            console.log(`catch create room #2`);
+            await createRoom(roomName);
             console.log(`joined the room #2`);
         } else {
-            console.log(`error code ${error.code}`);
+            console.log(`error findOrCreateRoom code ${error.code}`);
             // let other errors bubble up
             // throw error;
             findOrCreateRoom(roomName);
         }
     }
 };
+
+/*separate try from findOrCreateRoom because cannot be caught on findOrCreateRoom itself*/
+const createRoom = async (roomName) => {
+    try {
+        console.log(`try create the room ${roomName}`);
+        await twilioClient.video.rooms.create({
+            uniqueName: roomName,
+            type: "go",
+        });
+    } catch (error) {
+        console.log(`catch create the room, wont retry. reason ${error} :: ${error.code}`);
+        if (error.code !== 53113){
+        console.log(`create room ${roomName}`);
+            await createRoom(roomName);
+        }
+    }
+}
 
 // An Access Token gives a participant permission to join video rooms.
 const getAccessToken = (roomName) => {
@@ -77,10 +93,14 @@ app.post("/join-room", async (req, res) => {
     // find or create a room with the given roomName
     findOrCreateRoom(roomName);
     // generate an Access Token for a participant in this room
+    console.log('findOrCreateRoom')
     const token = getAccessToken(roomName);
+    console.log('getAccessToken')
+
     res.send({
         token: token,
     });
+    console.log('res.send token')
 });
 
 // serve static files from the public directory
